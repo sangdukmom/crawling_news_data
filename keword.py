2c86cc905aba53a3a835102deffb73261d2fa45a
sangdukmom <116548265+sangdukmom@users.noreply.github.com>
2025-03-12 10:29:56 +0900

    Create keword.py

---
+++ keword.py
@@ -0,0 +1,22 @@
+DRAM
+NAND
+HBM
+TSMC
+SMIC
+Globalfoundries
+Automotive
+Electric Vehicle
+Smartphone
+PC
+Notebook
+Server
+AI Server
+Semiconductor
+Microsoft
+Google
+AWS
+Meta
+CoreWeave
+Nvidia
+openAI
+SiC
